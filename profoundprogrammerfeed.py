de9a7879709e1378891b5a3490407358b9fec556
Thijs de Zoute <thijsdezoete@gmail.com>
2012-08-23 10:54:27 +0200

    blah

--- profoundprogrammerfeed.py
+++ profoundprogrammerfeed.py
@@ -7,17 +7,18 @@ class Profoundprogrammer(HTMLParser):
     hd_image_detected = False
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'a':
+        if tag == 'img':
             for attr in attrs:
                 (attr_name, attr_value) = attr
-                if (attr_name == 'href' and self.hd_image_detected == True):
+                print "tag: %s attr: %s" % (tag, attr_name)
+                if (attr_name == 'src'):
                     print 'img attr: %s' % attr_value
                     self.images.append(attr_value)
 
         #print "Encountered a start tag:", tag
 
-    def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
+    #def handle_endtag(self, tag):
+        #print "Encountered an end tag :", tag
 
     def handle_data(self, data):
         if 'HD' in data:
@@ -27,3 +28,7 @@ class Profoundprogrammer(HTMLParser):
         else:
             #self.remove_link()
             pass
+
+    def close(self):
+        print 'TEST!'
+        return self.images
--- profprog_ripper.py
+++ profprog_ripper.py
@@ -6,11 +6,16 @@ feed = feedparser.parse(profoundprog_url)
 
 
 for item in feed['items']:
-    print item['title']
+    print "Entry title: %s" % item['title']
     if '[text:' in item['title']:
         #print item
         item_parser = Profoundprogrammer()
+
         test = item_parser.feed(item['summary'])
-        print test
-        quit()
+        #print test
+        #quit()
         print 'this item has an image'
+images = item_parser.close()
+
+for image in images:
+    print 'image: %s' % image
